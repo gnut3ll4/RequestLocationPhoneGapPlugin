6344cbcbce54ae0e9a5c9868fae64deb182289cc
Thibaut Tauveron <thibaut.tauveron@solutioncondo.com>
2014-11-24 18:21:43 -0500

    .

--- www/requestlocation.js
+++ www/requestlocation.js
@@ -1,12 +1,29 @@
-var requestLocation = {
-    enableLocation: function(successCallback, errorCallback) {
-        cordova.exec(
+    var RequestLocationLoader = function (require) {
+
+        var exec = require("cordova/exec");
+
+        function RequestLocation() {};
+
+        RequestLocation.prototype.enableLocation = function (successCallback, errorCallback) {
+            exec(
             successCallback, // success callback function
             errorCallback, // error callback function
-            'RequestLocation', // mapped to our native Java class called "Calendar"
+            'RequestLocation', // mapped to our native Java class called "RequestLocation"
             'enableLocation' // with this action name
-        );
-    }
-}
+			);
+        };
 
-module.exports = requestLocation;
+       
+        var requestLocation = new RequestLocation();
+        module.exports = requestLocation;
+
+    }
+
+    RequestLocationLoader(require);
+
+    cordova.define("cordova/plugin/RequestLocation", RequestLocationLoader);
+
+
+
+
+
